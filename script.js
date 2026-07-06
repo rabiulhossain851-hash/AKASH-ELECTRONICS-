a2f6f161f4f3d8f9c570fdd58b38d86505c1c71c
rabiulhossain851-hash <rabiulhossain851@gmail.com>
2026-07-07 00:44:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ product.style.display = "none";
 }
 
 });
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+if (slides.length > 0) {
+
+    setInterval(function () {
+
+        slides[current].classList.remove("active");
+
+        current++;
+
+        if (current >= slides.length) {
+            current = 0;
+        }
+
+        slides[current].classList.add("active");
+
+    }, 3000);
+
+}
